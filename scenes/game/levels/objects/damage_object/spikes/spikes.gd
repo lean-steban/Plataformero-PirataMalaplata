144e58227b51afe7df75593c02b99a35d4511072
lean-steban <leandrocuz26@gmail.com>
2024-06-14 00:08:42 -0500

    CAMBIO 5

--- scenes/game/levels/objects/damage_object/spikes/spikes.gd
+++ scenes/game/levels/objects/damage_object/spikes/spikes.gd
@@ -1,13 +1,11 @@
-extends Node
-## Script para objeto que hace daño "spike"
-##
-## Al entrar en contacto con este objeto, el personaje pierda toda la vida
-
+extends Node2D
 
 var _player_script: Node2D # Referencia al script del jugador, para poder bajar vida
+var _punch_sound = preload("res://assets/sounds/punch.mp3")
+var _male_hurt_sound = preload("res://assets/sounds/male_hurt.mp3")
 
 @onready var _timer = $Timer # Variable para temporizar el daño
-
+@onready var _audio_player = $HurtSoundPlayer # Reproductor de audios
 
 # Escuchamos cuando un "cuerpo" entra en el área de contacto
 func _on_area_body_entered(body):
@@ -15,15 +13,17 @@ func _on_area_body_entered(body):
 		_player_script = body.get_node("MainCharacterMovement")
 		# "Golpeamos" al personaje
 		_player_script.hit(2)
+		# Reproducimos el sonido de daño
+		if _audio_player and _male_hurt_sound:
+			_audio_player.stream = _male_hurt_sound
+			_audio_player.play()
 		# Iniciamos el temporizador
 		_timer.start()
-
 
 # Escuchamos cuando un "cuerpo" sale del área de contacto
 func _on_area_body_exited(body):
 	# Al salir de los "picos", borramos la variable para no seguir haciendo daño
 	_player_script = null
-
 
 # Escuchamos cuando se acaba el temporizador
 func _on_timer_timeout():
@@ -35,5 +35,7 @@ func _on_timer_timeout():
 		return
 	# Cuando termina el temporizador, hacemos daño al jugador
 	_player_script.hit(2)
-	# Luego procedemos a inicializar de nuevo el temporizador
-	_timer.start()
+	# Reproducimos el sonido de daño
+	if _audio_player and _male_hurt_sound:
+		_audio_player.stream = _male_hurt_sound
+		_audio_player.play()
